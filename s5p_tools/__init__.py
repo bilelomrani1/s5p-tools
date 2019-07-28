b64c2540c4a7492ea6b4875aa9bb10155dd20d6a
Bilel Omrani <bilel.omrani@ecl17.ec-lyon.fr>
2019-07-29 00:02:19 +0200

    Update HARP commands

--- s5p_tools/__init__.py
+++ s5p_tools/__init__.py
@@ -1,3 +1,2 @@
-from . import map_tools
 from . import pretty_print
 from . import s5p_tools
--- s5p_tools/map_tools/__init__.py
+++
@@ -1,2 +0,0 @@
-from .scalebar import scale_bar
-from .colorbar import color_bar, adjust_color_bar_to_plot
